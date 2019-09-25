917e95478965b1a35062e30adf65d25be5ca46b3
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-09-26 00:10:37 +0200

    refactor(homesickness index calculator): make it autumn-compatible

--- Services/indexCalculator.cs
+++ Services/indexCalculator.cs
@@ -6,13 +6,15 @@ namespace HomesicknessVisualiser.Services
     {
         public static int getIndex(double bpTemp, double csTemp)
         {
-            if (bpTemp > 24 && bpTemp > csTemp) {                                           // Causes for homesickness:      
-                return Convert.ToInt32(Math.Pow(bpTemp - csTemp, 2) * (bpTemp - 24));       // - when summers are too warm
+            if (bpTemp > 21 && bpTemp > csTemp) {                                           // Causes for homesickness:      
+                return Convert.ToInt32(Math.Pow(bpTemp - csTemp, 3) * (bpTemp - 21));       // - when summers are too warm
             }
-            else if (bpTemp < 12 && bpTemp > 0 && csTemp < 0)
+            
+            if (bpTemp < 16 && bpTemp > 0 && csTemp < 0)
             {
-                return Convert.ToInt32(Math.Pow(bpTemp - csTemp, 2) * (bpTemp));            // - when winters are cold, but not cold enough for snow
+                return Convert.ToInt32(Math.Pow(bpTemp - csTemp, 2) * (bpTemp));            // - when winters are not cold enough for snow
             }
+
             return 0;
         }
     }
